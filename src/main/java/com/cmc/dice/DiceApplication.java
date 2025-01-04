72521c01b507123aaa53bb78665a349f88613610
김민규 <min1925k@gmail.com>
2025-01-05 01:23:43 +0900

    Initial Commit

---
+++ src/main/java/com/cmc/dice/DiceApplication.java
@@ -0,0 +1,17 @@
+package com.cmc.dice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
+public class DiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceApplication.class, args);
+	}
+
+}
