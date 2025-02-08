9fe963ef5f9d2304e4bc114e7aa53365dcbfaf85
김민규 <min1925k@gmail.com>
2025-02-08 16:42:08 +0900

    docs: Guest 정보 샘플 추가

--- src/main/java/com/cmc/dice/domain/brand/dto/SimpleBrandInfoDto.java
+++ src/main/java/com/cmc/dice/domain/brand/dto/SimpleBrandInfoDto.java
@@ -1,6 +1,7 @@
 package com.cmc.dice.domain.brand.dto;
 
 import com.cmc.dice.domain.brand.domain.Brand;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SimpleBrandInfoDto {
+	@Schema(description = "브랜드 ID", example = "1")
 	private Long id;
+	@Schema(description = "브랜드 이름", example = "브랜드 이름")
 	private String name;
+	@Schema(description = "로고 URL", example = "https://example.com/logo.jpg")
 	private String logoUrl;
 
 	public static SimpleBrandInfoDto of(Brand brand) {
