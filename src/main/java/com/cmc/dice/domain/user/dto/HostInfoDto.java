3e1196bbcf16ea09986900920a160509af553b49
김민규 <min1925k@gmail.com>
2025-02-08 16:43:02 +0900

    docs: 호스트 정보 샘플

--- src/main/java/com/cmc/dice/domain/user/dto/HostInfoDto.java
+++ src/main/java/com/cmc/dice/domain/user/dto/HostInfoDto.java
@@ -2,6 +2,7 @@ package com.cmc.dice.domain.user.dto;
 
 import com.cmc.dice.domain.brand.dto.SimpleBrandInfoDto;
 import com.cmc.dice.domain.user.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,14 +13,19 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HostInfoDto {
+	@Schema(description = "사용자 이름", example = "호스트")
 	private String name;
 
+	@Schema(description = "사용자 이메일", example = "enterprize@enterprize.com")
 	private String email;
 
+	@Schema(description = "사용자 휴대폰 번호", example = "01012345678")
 	private String phone;
 
+	@Schema(description = "은행 이름", example = "신한은행")
 	private String bankName;
 
+	@Schema(description = "계좌 번호", example = "1234567890")
 	private String accountNumber;
 
 	public static HostInfoDto of(User user) {
