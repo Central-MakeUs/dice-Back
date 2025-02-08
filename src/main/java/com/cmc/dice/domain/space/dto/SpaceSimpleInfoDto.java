de15bc166cda76f8e92676d770483eceb3a76b12
김민규 <min1925k@gmail.com>
2025-02-08 15:41:42 +0900

    docs : 샘플 및 좋아요 여부, 채팅방 ID 추가

--- src/main/java/com/cmc/dice/domain/space/dto/SpaceSimpleInfoDto.java
+++ src/main/java/com/cmc/dice/domain/space/dto/SpaceSimpleInfoDto.java
@@ -1,7 +1,9 @@
 package com.cmc.dice.domain.space.dto;
 
 import com.cmc.dice.domain.space.domain.Space;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.locationtech.jts.geom.Point;
@@ -9,31 +11,58 @@ import org.locationtech.jts.geom.Point;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SpaceSimpleInfoDto {
+	@Schema(description = "공간 ID", example = "1")
 	private Long id;
+
+	@Schema(description = "공간 이름", example = "공간 이름")
 	private String name;
+
+	@Schema(description = "공간 주소", example = "서울시 강남구 테헤란로 123")
 	private String address;
+
+	@Schema(description = "공간 이미지 URL", example = "www.example.com")
 	private String imageUrl;
 
+	@Schema(description = "일일 가격", example = "10000")
 	private int pricePerDay;
+
+	@Schema(description = "할인율", example = "10")
 	private int discountRate;
+
+	@Schema(description = "할인 가격", example = "9000")
 	private int discountPrice;
+
+	@Schema(description = "수용 인원", example = "10")
 	private int capacity;
 
+	@Schema(description = "공간 크기", example = "30")
+	private int size;
+
+	@Schema(description = "좋아요 수", example = "10")
 	private int likeCount;
 
+	@Schema(description = "좋아요 여부", example = "true")
+	@Builder.Default
+	private boolean isLiked = false;
+
+	@Schema(description = "채팅방 ID", example = "1")
+	@Builder.Default
+	private Long chatRoomId = null;
 
 	public static SpaceSimpleInfoDto of(Space space) {
-		return new SpaceSimpleInfoDto(
-				space.getId(),
-				space.getName(),
-				space.getCity() + " " + space.getDistrict() + " " + space.getAddress(),
-				space.getImageUrls().get(0),
-				space.getPricePerDay(),
-				space.getDiscountRate(),
-				space.getDiscountPrice(),
-				space.getCapacity(),
-				space.getLikeCount()
-		);
+		return SpaceSimpleInfoDto.builder()
+				.id(space.getId())
+				.name(space.getName())
+				.address(space.getAddress())
+				.imageUrl(space.getImageUrls().get(0))
+				.pricePerDay(space.getPricePerDay())
+				.discountRate(space.getDiscountRate())
+				.discountPrice(space.getDiscountPrice())
+				.capacity(space.getCapacity())
+				.size(space.getSize())
+				.likeCount(space.getLikeCount())
+				.build();
 	}
 }
