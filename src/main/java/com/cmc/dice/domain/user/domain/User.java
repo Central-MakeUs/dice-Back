40e822e4974658228084123eeefcf27e3854a81c
김민규 <min1925k@gmail.com>
2025-02-09 02:17:58 +0900

    feat: uniqe 조건 추가

--- src/main/java/com/cmc/dice/domain/user/domain/User.java
+++ src/main/java/com/cmc/dice/domain/user/domain/User.java
@@ -31,7 +31,7 @@ public class User extends BaseEntity {
     @Convert(converter = UserRoleConverter.class)
     private UserRole userRole;
 
-    @Column(nullable = false)
+    @Column(unique = true, nullable = false)
     private String phone;
 
     private String bankName;
