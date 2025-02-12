6fcb889872c703751cf0ad0d8da95120f9062f92
min9805 <56664567+min9805@users.noreply.github.com>
2025-02-13 01:33:59 +0900

    [DICE-20] S3 업로드 설정 (#22)
    * feat: aws 의존성 추가
    
    * feat: S3 설정 추가
    
    * feat: Upload 로직, 컨트롤러 추가
    
    * feat: minCapacity 추가
    
    * feat: 다수 파일 업로드 추가

---
+++ src/main/java/com/cmc/dice/global/infra/S3Controller.java
@@ -0,0 +1,43 @@
+package com.cmc.dice.global.infra;
+
+import com.cmc.dice.domain.user.domain.User;
+import com.cmc.dice.global.jwt.CurrentUser;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/s3")
+@RequiredArgsConstructor
+@Tag(name = "S3")
+public class S3Controller {
+    private final S3Uploader s3Uploader;
+
+    @PostMapping(value = "/upload", consumes = {"multipart/form-data"})
+    @PreAuthorize("isAuthenticated()")
+    @SecurityRequirement(name = "access-token")
+    public ImageUrlDto uploadImage(
+            @CurrentUser User user,
+            @RequestBody MultipartFile file) {
+        String imageUrl = s3Uploader.saveFile(user, file);
+        return new ImageUrlDto(imageUrl);
+    }
+
+    @PostMapping(value = "/uploads", consumes = {"multipart/form-data"})
+    @PreAuthorize("isAuthenticated()")
+    @SecurityRequirement(name = "access-token")
+    public ImageUrlDtos uploadImages(
+            @CurrentUser User user,
+            @RequestBody MultipartFile[] files) {
+
+        List<String> urls = s3Uploader.saveFiles(user, Arrays.stream(files).toList());
+        return new ImageUrlDtos(urls);
+    }
+}
