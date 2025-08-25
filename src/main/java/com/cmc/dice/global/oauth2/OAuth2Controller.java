f6776505441c13b33c63101a44e8cb46094b6bdc
singsangssong <plmko0914@gmail.com>
2025-08-25 20:16:31 +0900

    FIX: fix socialLoginUrl

--- src/main/java/com/cmc/dice/global/oauth2/OAuth2Controller.java
+++ src/main/java/com/cmc/dice/global/oauth2/OAuth2Controller.java
@@ -1,8 +1,10 @@
 package com.cmc.dice.global.oauth2;
 
+import com.cmc.dice.global.oauth2.dto.common.SocialLoginUrlResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,11 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class OAuth2Controller {
     @GetMapping("/{socialType}")
-    public void socialLoginRedirect (@PathVariable(name = "socialType") String socialType,
-                                     HttpServletResponse response) throws IOException {
-        response.sendRedirect("https://diceminipop.site/oauth2/authorization/" + socialType);
+    public ResponseEntity<SocialLoginUrlResponse> socialLoginRedirect (
+            @PathVariable(name = "socialType") String socialType,
+            HttpServletResponse response) throws IOException {
+        String socialLoginUrl = "https://diceminipop.site/oauth2/authorization/" + socialType;
 //        response.sendRedirect("http://localhost:8080/oauth2/authorization/" + socialType);
+        return ResponseEntity.ok(new SocialLoginUrlResponse(socialLoginUrl));
     }
 }
